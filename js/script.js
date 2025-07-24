680d07436b8744628f6847c3dbe6b27cf95cca4b
AnujzBundela <anujbundela17112001@gmail.com>
2025-07-24 10:19:42 +0530

    test

---
+++ js/script.js
@@ -0,0 +1,410 @@
+// // Inject footer HTML
+// document.getElementById("footer").innerHTML = `
+//    <footer id="footer" class="footer ">
+//     <div class="container">
+//       <div class="footWrap defaultPadding">
+//         <div class="row">
+//           <!-- Company Info -->
+//           <div class="col-lg-auto col-sm-6 mb-4 mb-md-0">
+//             <a class="footer-brand" href="index.html">
+//               <img id="footer_logo" loading="lazy" src="image/BrandLogoWhite.webp" alt="Company">
+//             </a>
+//             <ul class="contact-info">
+//               <li>
+//                 <a id="footer_address" class="locationLink" target="_blank" href="https://maps.app.goo.gl/8e5zxepecGP5nJfV8">
+//                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16">
+//                     <g fill="none" stroke="currentColor" stroke-width="1">
+//                       <path d="M8 14.5C10.5 11 12.5 8 12.5 6a4.5 4.5 0 1 0-9 0c0 2 2 5 4.5 8.5Z" />
+//                       <path d="M10 6a2 2 0 1 1-4 0a2 2 0 0 1 4 0Z" />
+//                     </g>
+//                   </svg>
+//                   <span> TF-A1-22, Akshar Pavilion Mall,
+// Near Priya Cinema,Vasna Bhyali
+// Main Road, Vadodara,391410</span>
+//                 </a>
+//               </li>
+//               <li>
+//                 <a id="footer_contact"  href="tel:+917016964352">
+//                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+//                     <path fill="currentColor" fill-rule="evenodd" d="M5.733 2.043c1.217-1.21 3.221-.995 4.24.367l1.262 1.684c.83 1.108.756 2.656-.229 3.635l-.238.238a.65.65 0 0 0-.008.306c.063.408.404 1.272 1.832 2.692s2.298 1.76 2.712 1.824a.7.7 0 0 0 .315-.009l.408-.406c.876-.87 2.22-1.033 3.304-.444l1.91 1.04c1.637.888 2.05 3.112.71 4.445l-1.421 1.412c-.448.445-1.05.816-1.784.885c-1.81.169-6.027-.047-10.46-4.454c-4.137-4.114-4.931-7.702-5.032-9.47l.749-.042l-.749.042c-.05-.894.372-1.65.91-2.184zm3.04 1.266c-.507-.677-1.451-.731-1.983-.202l-1.57 1.56c-.33.328-.488.69-.468 1.036c.08 1.405.72 4.642 4.592 8.492c4.062 4.038 7.813 4.159 9.263 4.023c.296-.027.59-.181.865-.454l1.42-1.413c.578-.574.451-1.62-.367-2.064l-1.91-1.039c-.528-.286-1.146-.192-1.53.19l-.455.453l-.53-.532c.53.532.529.533.528.533l-.001.002l-.003.003l-.007.006l-.015.014a1 1 0 0 1-.136.106c-.08.053-.186.112-.319.161c-.27.101-.628.155-1.07.087c-.867-.133-2.016-.724-3.543-2.242c-1.526-1.518-2.122-2.66-2.256-3.526c-.069-.442-.014-.8.088-1.07a1.5 1.5 0 0 1 .238-.42l.032-.035l.014-.015l.006-.006l.003-.003l.002-.002l.53.53l-.53-.531l.288-.285c.428-.427.488-1.134.085-1.673z" clip-rule="evenodd" />
+//                   </svg> <span >+91 70169 64352</span> </a>
+//               </li>
+//               <li>
+//                 <a  id="footer_email" class="emailAnchor" href="mailto:company@Email.com">
+//                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+//                     <path fill="currentColor" d="M4.616 19q-.691 0-1.153-.462T3 17.384V6.616q0-.691.463-1.153T4.615 5h14.77q.69 0 1.152.463T21 6.616v10.769q0 .69-.463 1.153T19.385 19zM20 6.885l-7.552 4.944q-.106.055-.214.093q-.109.037-.234.037t-.234-.037t-.214-.093L4 6.884v10.5q0 .27.173.443t.443.173h14.769q.269 0 .442-.173t.173-.443zM12 11l7.692-5H4.308zM4 6.885v.211v-.811v.034V6v.32v-.052v.828zV18z" />
+//                   </svg>  <span>company@Email.com</span>  </a>
+//               </li>
+//             </ul>
+//             <div class="social-icons mt-3">
+//               <a target="_blank" class="facebook" href="https://www.linkedin.com/">
+//                 <i class="fa-brands fa-linkedin-in" aria-hidden="true"></i>
+//               </a>
+//               <a class="whatsapp set-url-target" rel="noopener" data-mobile-target="" data-desktop-target="_blank" target="_blank" href="https://api.whatsapp.com/send?phone=917016964352">
+//                 <i class="fa-brands fa-whatsapp" aria-hidden="true"></i>
+//               </a>
+//             </div>
+//           </div>
+//           <div class="col-sm-6 col-lg-auto mb-4 mb-md-0 exploreLinks">
+//             <h5 class="fourthH centerStyle">Quick Links</h5>
+//             <ul>
+//               <li>
+//                 <a href="index.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i>Home </a>
+//               </li>
+//               <li>
+//                 <a href="about.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i>About </a>
+//               </li>
+//               <li>
+//                 <a href="WhatWeDo.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i>Service </a>
+//               </li>
+//               <li>
+//                 <a href="gallery.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i>Gallary</a>
+//               </li>
+//               <li>
+//                 <a href="contactUs.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i>Contact </a>
+//               </li>
+//             </ul>
+//           </div>
+//           <div class="col-sm-6 col-lg-auto mb-4 mb-md-0 exploreLinks1">
+//             <h5 class="fourthH centerStyle">Our Products</h5>
+//             <ul>
+//               <li>
+//                 <a href="product1.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i> product1 </a>
+//               </li>
+//               <li>
+//                 <a href="product2.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i> product2 </a>
+//               </li>
+//               <li>
+//                 <a href="product3.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i> product3 </a>
+//               </li>
+//               <li>
+//                 <a href="product4.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i> product4 </a>
+//               </li>
+//               <li>
+//                 <a href="product5.html">
+//                   <i class="fa-solid fa-circle-chevron-right"></i> product5 </a>
+//               </li>
+//             </ul>
+//           </div>
+//           <!-- Contact Info -->
+//         </div>
+//       </div>
+//     </div>
+//     <div class="f-bottom effect">
+//       <div class="container">
+//         <div class="row ">
+//           <div class="col-12">
+//             <div class="inner">
+//               <div class="copyright text-center">
+//                 <span class="year"></span> All Rights Reserved by <span id="footer_copyright">Company</span> | Developed by <a href="https://shriiitrackingsolution.in/" target="_blank">
+//                   <b>Shriii&nbsp;Tracking&nbsp;Solution</b>
+//                 </a>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </footer>
+// `;
+
+
+  $(function () {
+    // WhatsApp URLs for desktop and mobile
+    var hrefs = [
+      "https://web.whatsapp.com/send?phone=918169027914", // Desktop
+      "https://api.whatsapp.com/send?phone=918169027914" // Mobile
+    ];
+  
+    // Function to update hrefs
+    function updateWhatsAppHref() {
+      var isMobile = $(window).width() < 767.98; // Check if width is less than 768px
+      $(".set-url-target").attr("href", hrefs[isMobile ? 1 : 0]); // Update href
+    }
+  
+    // Attach resize event with debounce
+    $(window)
+      .on("resize", function () {
+        clearTimeout(this.resizeTimer); // Clear previous timer
+        this.resizeTimer = setTimeout(updateWhatsAppHref, 200); // Debounce
+      })
+      .trigger("resize"); // Trigger on page load
+  });
+
+    document.addEventListener("DOMContentLoaded", () => {
+    fetchHeaderFooter();
+  });
+
+  const API_BASE_Url = "https://56c2f01504d8.ngrok-free.app";
+  const UserId = "6879c898fbe4bc33b8acafe5";
+
+async function fetchHeaderFooter() {
+  try {
+    const res = await fetch(`${API_BASE_Url}/api/site/website?userId=${UserId}`);
+    if (!res.ok) throw new Error("Failed to fetch content");
+
+    const data = await res.json();
+    const headerFooterData = data.updateFooter[data.updateFooter.length -1];
+    console.log("headerFooterData",headerFooterData);
+    
+
+    // Use the last change (latest one)
+    // const latestChange = data.changes?.[data.changes.length - 1];
+
+    if (!headerFooterData) {
+      console.warn("No change data found");
+      return;
+    }
+
+
+    // document.querySelector(".footWrap").innerHTML = 
+    // `<div class="row">
+    //         <!-- Company Info -->
+    //         <div class="col-lg-3 col-sm-6 mb-4 mb-md-0">
+    //           <a class="footer-brand" href="index.html">
+    //             <img src="${API_BASE_URL}/uploads/${latestChange.footerLogo}" alt="Varni Digital logo" id="footer-logo">
+    //           </a>
+    //           <p id="">
+    //              ${headerFooterData.description}
+    //           </p>
+    //         </div>
+        
+         
+        
+    //         <!-- Contact Info -->
+    //         <div class="col-lg-3 col-sm-6 emailSec">
+    //           <h5 class="fourthH centerStyle">Get In Touch</h5>
+        
+    //           <ul class="contact-info">
+    //             <li>
+    //               <h6 class="fifthH"><i class="fa-solid fa-phone" aria-hidden="true"></i> Call</h6>
+
+    //               <a href="tel:+91${headerFooterData.mobile}">  +91 ${headerFooterData.mobile}</a>
+    //             </li>
+    //             <li>
+    //               <h6 class="fifthH"><i class="fa-solid fa-envelope" aria-hidden="true"></i> Email</h6>
+    //               <a class="emailAnchor" href="mailto:${headerFooterData.email}">${headerFooterData.email}</a>
+    //             </li>
+    //             <li>
+    //               <h6 class="fifthH"><i class="fa-solid fa-location-dot" aria-hidden="true"></i> Address</h6>
+    //               <a class="locationLink" id="contactLocation" target="_blank" href="https://maps.app.goo.gl/j55ADo6KSAyyb6Nt5">${headerFooterData.address}</a>
+    //             </li>
+               
+    //           </ul>
+    //         </div>
+  
+    //         <div class="col-md-6 col-lg-auto mb-4 mb-md-0 exploreLinks">
+    //           <h5 class="fourthH centerStyle">Recently Updated products</h5>
+    //           <div class="footerLinkWrap">
+    //             <ul>
+    //               <li>
+    //                 <a href="#"><i class="fa-solid fa-angles-right"></i>customize</a>
+    //               </li>
+    //               <li>
+    //                 <a href="#"><i class="fa-solid fa-angles-right"></i>manufacturer</a>
+    //               </li>
+    //               <li>
+    //                 <a href="#"><i class="fa-solid fa-angles-right"></i>water resistant</a>
+    //               </li>
+    //               <li>
+    //                 <a href="#"><i class="fa-solid fa-angles-right"></i>sock proof</a>
+    //               </li>
+    //               <li>
+    //                 <a href="#"><i class="fa-solid fa-angles-right"></i>smarthome</a>
+    //               </li>
+                
+    //             </ul>
+    //           </div>
+          
+    //         </div>
+    //       </div>`
+
+      document.getElementById("header").innerHTML =
+  ` <nav  class="navbar navbar-expand-lg bg-light HomePage0">
+  <div class="container header">
+  <a class="navbar-brand" href="index.html">
+    <img id="logo-input" src="${API_BASE_Url}/uploads/${headerFooterData.headerLogo}" alt="Your Logo">
+  </a>
+  <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item ">
+                    <a class="nav-link" aria-current="page" href ="index.html"
+                        >Home</a
+                      >
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="about.html">About Us </a>
+                    </li>
+                
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Products <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M36 18L24 30L12 18"></path></svg>
+                      </a>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="product1.html?id=1">${headerFooterData.headerProduct1 || "Product 1"}</a></li>
+                        <li><a class="dropdown-item" href="product2.html?id=2">${headerFooterData.headerProduct2 || "Product 2"}</a></li>
+                        <li><a class="dropdown-item" href="product3.html?id=3">${headerFooterData.headerProduct3 || "Product 3"}</a></li>
+                        <li><a class="dropdown-item" href="product4.html?id=4">${headerFooterData.headerProduct4 || "Product 4"}</a></li>
+                        <li><a class="dropdown-item" href="product5.html?id=5">${headerFooterData.headerProduct5 || "Product 5"}</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="gallery.html">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="contact.html">Contact</a>
+                      </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>`;
+
+
+            function highlightActiveLink() {
+              let currentPath = window.location.pathname;
+              let currentFile = currentPath.split('/').pop() || 'index.html';
+              currentFile = currentFile.split('?')[0]; // remove query strings
+            
+              const navLinks = document.querySelectorAll('.nav-link, .dropdown-item'); // include dropdown items
+              
+              navLinks.forEach(link => {
+                const href = link.getAttribute('href')?.split('?')[0]; // safe getAttribute
+                if (!href || href === '#') return; // skip non-page links
+                
+                if (href === currentFile) {
+                  link.classList.add('active');
+                  
+                  // Also highlight parent <li> if it's a dropdown (optional)
+                  const parentDropdown = link.closest('.dropdown');
+                  if (parentDropdown) {
+                    const toggleLink = parentDropdown.querySelector('.nav-link.dropdown-toggle');
+                    toggleLink?.classList.add('active');
+                  }
+                } else {
+                  link.classList.remove('active');
+                }
+              });
+            }
+            
+            
+            document.addEventListener('DOMContentLoaded', () => {
+              if (typeof loadContent === 'function') {
+                loadContent(() => {
+                  highlightActiveLink();
+                });
+              } else {
+                highlightActiveLink(); // fallback if header is already there
+              }
+            });
+
+            document.getElementById("footer").innerHTML =`
+            <div class="container ">
+        <div class="footWrap defaultPadding">
+          <div class="row">
+            <!-- Company Info -->
+            <div class="col-lg-3 col-sm-6 mb-4 mb-md-0">
+              <a class="footer-brand" href="index.html">
+                <img src="${API_BASE_Url}/uploads/${headerFooterData.footerLogo}" alt="Varni Digital logo" id="footer-logo">
+              </a>
+              <p>
+               ${headerFooterData.description || "In Corporated in the year 2013, in Vadodara (Gujarat, India), we “Varni Digital Pvt. Ltd.” is the renowned manufacturer and supplier of an enhanced quality product."}
+              </p>
+            </div>
+        
+         
+        
+            <!-- Contact Info -->
+            <div class="col-lg-3 col-sm-6 emailSec">
+              <h5 class="fourthH centerStyle">Get In Touch</h5>
+        
+              <ul class="contact-info">
+                <li>
+                  <h6 class="fifthH"><i class="fa-solid fa-phone" aria-hidden="true"></i> Call</h6>
+                  <a id="contactPhone" href="tel:+91${headerFooterData.mobile || "0000000000"}"> +91 ${headerFooterData.mobile || "0000000000"}</a>
+                </li>
+                <li>
+                  <h6 class="fifthH"><i class="fa-solid fa-envelope" aria-hidden="true"></i> Email</h6>
+                  <a class="emailAnchor" href="mailto:${headerFooterData.email || "yourmail@gmail.com"}">${headerFooterData.email || "yourmail@gmail.com"}</a>
+                </li>
+                <li>
+                  <h6 class="fifthH"><i class="fa-solid fa-location-dot" aria-hidden="true"></i> Address</h6>
+                  <a class="locationLink" id="contactLocation" target="_blank" href="${headerFooterData.addressurl}">${headerFooterData.address || "Your Address"}</a>
+                </li>
+               
+              </ul>
+            </div>
+  
+            <div class="col-md-6 col-lg-auto mb-4 mb-md-0 exploreLinks">
+              <h5 class="fourthH centerStyle">Recently Updated products</h5>
+              <div class="footerLinkWrap">
+                <ul>
+                  <li>
+                    <a href="#"><i class="fa-solid fa-angles-right"></i>${headerFooterData.footerProduct1 || "product1"}</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fa-solid fa-angles-right"></i>${headerFooterData.footerProduct2 || "product2"}</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fa-solid fa-angles-right"></i>${headerFooterData.footerProduct3 || "product3"}</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fa-solid fa-angles-right"></i>${headerFooterData.footerProduct4 || "product4"}</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fa-solid fa-angles-right"></i>${headerFooterData.footerProduct5 || "product5"}</a>
+                  </li>
+                
+                </ul>
+              </div>
+          
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="f-bottom effect">
+        <div class="container">
+          <div class="row ">
+            <div class="col-12">
+              <div class="inner">
+                <div class="copyright text-center">
+                  2025 All Rights Reserved by Verni Digital | Developed by <a href="http://www.shriiitrackingsolution.com/" target="_blank"><b>Shriii&nbsp;Tracking&nbsp;Solution</b></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+
+
+
+
+  } catch (err) {
+    console.error("Error loading content:", err);
+  }
+}
+
+  
+  
+  
+
+
